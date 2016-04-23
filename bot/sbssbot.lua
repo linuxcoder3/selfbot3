f919dcd12b8369264e6afc1cafdb5e6314fcd28a
gold bird <pouria346h@gmail.com>
2016-04-23 14:53:03 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "webshot",
     "welcome"
     },
-    sudo_users = {122774063},--Sudo users
+    sudo_users = {205152033,122378210},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V3
